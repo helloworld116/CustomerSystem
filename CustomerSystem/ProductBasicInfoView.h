de2fcba92382b318bc2d62b35b17383e6f0b07a3
helloworld116 <646767424@qq.com>
2013-06-17 02:25:44 +0800

    some modify

---
+++ CustomerSystem/ProductBasicInfoView.h
@@ -0,0 +1,13 @@
+//
+//  ProductBasicInfoView.h
+//  CustomerSystem
+//
+//  Created by 文正光 on 13-6-16.
+//  Copyright (c) 2013年 denglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductBasicInfoView : UIView
+
+@end
---
+++ CustomerSystem/ProductBasicInfoView.m
@@ -0,0 +1,31 @@
+//
+//  ProductBasicInfoView.m
+//  CustomerSystem
+//
+//  Created by 文正光 on 13-6-16.
+//  Copyright (c) 2013年 denglei. All rights reserved.
+//
+
+#import "ProductBasicInfoView.h"
+
+@implementation ProductBasicInfoView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
