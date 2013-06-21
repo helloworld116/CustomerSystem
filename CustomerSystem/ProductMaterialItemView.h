f6ef8e825ab0929b31d69b1a37f93d485c3d4b01
helloworld116 <646767424@qq.com>
2013-06-21 19:11:09 +0800

    6.21 commit

---
+++ CustomerSystem/ProductMaterialItemView.h
@@ -0,0 +1,13 @@
+//
+//  ProductMaterialItemView.h
+//  CustomerSystem
+//
+//  Created by wzg on 13-6-21.
+//  Copyright (c) 2013年 denglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductMaterialItemView : UIView
+
+@end
---
+++ CustomerSystem/ProductMaterialItemView.m
@@ -0,0 +1,31 @@
+//
+//  ProductMaterialItemView.m
+//  CustomerSystem
+//
+//  Created by wzg on 13-6-21.
+//  Copyright (c) 2013年 denglei. All rights reserved.
+//
+
+#import "ProductMaterialItemView.h"
+
+@implementation ProductMaterialItemView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
